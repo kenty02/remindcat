8c6467bd1bbb31518ef0252755bf41058f8e7dd2
kenty02 <42216299+kenty02@users.noreply.github.com>
2023-04-18 21:22:40 +0900

    add sentry

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import asyncio
-from config import port
+import os
 
+import sentry_sdk
 import uvicorn
 
 from api import app as app_fastapi
+from config import port
 from scheduler import app as app_rocketry
+
+sentry_sdk.init(
+    dsn="https://1bf65e21867e4356a57c11f3d6e90fef@o4504839999848448.ingest.sentry.io/4505034968203264",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+    environment=os.getenv("ENV", "dev"),
+)
 
 
 class Server(uvicorn.Server):
